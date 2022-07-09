d370884b7f9e46485035f8385a865e217553584c
Ricardo Texeira <ricardodeveloper@outlook.com>
2022-07-09 19:50:13 -0300

    Arrumando importações dos arquivos

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -11,7 +11,6 @@ export const Header = (props) => {
                            <span></span>
                         </h1>
                         <p>{props.data ? props.data.paragraph : "Loading"}</p>
-                        <p>{process.env.API_URL}</p>
                         <a href="#features" className="btn btn-custom btn-lg page-scroll">
                            Learn More
                         </a>{" "}
