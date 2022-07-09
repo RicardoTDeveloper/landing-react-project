578cecd5d5587d5efcd3afe236efccab85aecb94
Ricardo Texeira <ricardodeveloper@outlook.com>
2022-07-09 20:09:14 -0300

    Add variavel de ambiente

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -11,6 +11,7 @@ export const Header = (props) => {
                            <span></span>
                         </h1>
                         <p>{props.data ? props.data.paragraph : "Loading"}</p>
+                        <p>{import.meta.env.API_URL}</p>
                         <a href="#features" className="btn btn-custom btn-lg page-scroll">
                            Learn More
                         </a>{" "}
